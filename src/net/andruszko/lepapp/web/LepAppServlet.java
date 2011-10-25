2c67226b54df5fca95afc05732dcff84ad801402
Michal <wtk300@gmail.com>
2011-10-25 18:03:42 +0200

    init proj

---
+++ src/net/andruszko/lepapp/web/LepAppServlet.java
@@ -0,0 +1,16 @@
+package net.andruszko.lepapp.web;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+public class LepAppServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
---
+++ war/accessDenied.jsp
@@ -0,0 +1,14 @@
+<%@ taglib prefix='c' uri='http://java.sun.com/jstl/core_rt' %>
+
+<html>
+  <head>
+    <title>403</title>
+  </head>
+
+  <body>
+    <h1>Błąd 403</h1>
+
+    Nie dla psa kielbasa
+
+  </body>
+</html>
