8d9ac4973ecb8d73b52cdd23b9f9bd91cf1f1738
Michal <wtk300@gmail.com>
2012-02-10 21:34:32 +0100

    [new] dalsze poprawki

---
+++ war/js/app.js
@@ -0,0 +1,41 @@
+angular.module('lepModule', ['lepModule.services', 'lepModule.widgets'])
+.run(['$route','$rootScope', '$location', '$window', 'lepSessionService','securityService' , function($route,$rootScope, $location, $window,sessionService,securityService) {
+
+
+	  $route.when('/contact', {template: 'partials/contact.html', controller: ContactCtrl});
+	  $route.when('/home', {template: 'partials/home.html', controller: MyCtrl2});
+	  $route.when('/map', {template: 'partials/map.html', controller: MyCtrl2});
+	  $route.when('/login', {template: 'login.html', controller:LoginCtrl});
+	  $route.when('/register', {template: 'partials/register.html', controller:RegisterCtrl});
+	  $route.when('/aboutExam', {template: 'partials/aboutExam.html', controller:undefined});
+	  $route.when('/chooseExam', {template: 'partials/chooseExam.html', controller:LepExamCtrl});
+	  $route.when('/startExam', {template: 'secured/start.html', controller:ResolveExamCtrl});
+	  $route.when('/logout', {template: '/logout', controller:LogoutCtrl});
+	  $route.when('/logoutsuccess', {template: 'partials/logoutsuccess.html', controller:LogoutCtrl});
+	  $route.when('/loginerror', {template: 'partials/loginerror.html', controller:undefined});
+
+    var self = this;
+    
+    
+  	
+  	$route.otherwise({
+  	   redirectTo : "/login"
+  	});
+  	
+  	$route.parent($rootScope);
+
+
+	 $rootScope.lepItems = {};
+	 $rootScope.loggedUser = securityService.getLoggedUser();
+	
+	 $rootScope.refreshUser = function(){
+		self.loggedUser = securityService.getLoggedUser();
+	 }
+	
+	 $rootScope.sessions = sessionService.getSessionsInfo();
+	
+    
+    
+     
+     
+  }]);
